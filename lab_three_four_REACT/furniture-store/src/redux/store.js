94cb00d0cf63cfc662c6503ed518bdac8ca652c9
Maria Iudina <gretarichter@gmail.com>
2025-04-01 21:14:07 +0300

    added documentation

--- lab_three_four_REACT/furniture-store/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import filter from './slices/filterSlice'
-
-export const store = configureStore({ // создаем стор и передаем в него редюсер
-  reducer: { 
-    filter, // передаем редюсер
-},
-})
